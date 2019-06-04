a3b72faea2f6ca0d272329c7a6f70f1528e8d178
Michelle Dalmau <mdalmau@indiana.edu>
2019-06-04 14:32:04 -0400

    printing my address

---
+++ python/my_address.py
@@ -0,0 +1,12 @@
+my_address = {
+"first_name": "Michelle",
+"last_name": "Dalmau",
+"street": "3006 S. Olcott Blvd",
+"city": "Bloomington",
+"state": "Indiana",
+"zip": "47401"
+}
+
+print(my_address['first_name'] + " " + my_address['last_name'])
+print(my_address['street'])
+print(my_address['city'] + "," + my_address['state'] + "  " + my_address['zip'])
