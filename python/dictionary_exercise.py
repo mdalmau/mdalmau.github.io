21dbb25373086d1b7c0b53c76778d24bada45efa
Michelle Dalmau <mdalmau@indiana.edu>
2019-06-04 16:07:20 -0400

    updates

--- python/dictionary_exercise.py
+++ python/dictionary_exercise.py
@@ -4,3 +4,17 @@ my_info = {
 "hometown": "Miami, FL",
 "favorite_food": "Picadillo"
 }
+
+old_string = "Python is cool"
+new_string = old_string.upper()
+new_string.lower()
+
+favorite_color = "Black"
+not_favorite_color = "Yellow"
+
+veggies = ["carrots", "tomatoes", "celery"]
+my_veggies = [veggies[0],  veggies[1]]
+
+print("Doctor Who")
+doctors = ["Matt Smith", "David Tennet"]
+print(doctors[0])
