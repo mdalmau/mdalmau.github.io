44193c86b8fb86133d37be9b44ea5be4d2717313
Michelle Dalmau <mdalmau@indiana.edu>
2019-06-05 11:22:45 -0400

    more exercises

--- python/my_address.py
+++ python/my_address.py
@@ -1,15 +1,68 @@
-my_address = {
-"first_name": "Michelle",
-"last_name": "Dalmau",
-"street": "3006 S. Olcott Blvd",
-"city": "Bloomington",
-"state": "Indiana",
-"zip": "47401"
+##  Exercise COLLECTIONS
+# step 1: create the collections
+texts = {
+"Jane Eyre":"1847",
+"Antes que anochezca":"1992"
 }
+
+# use the items method for dictionaries to create an order
+# need to "name" the key:value pair so title and date (could be any words)
+for title, date in texts.items():
+    print(title + " was published in " + date + ".")
+
+
+##  METHOD that calls another METHOD
+def method_one():
+    print("look I'm a method")
+
+def method_two(name):
+    print("about to call our first method for " + name)
+    method_one()
+method_two("Michelle")
+
+def love():
+    print("I love them!")
+
+def family(name):
+    print("Let me introduce my family: " + name)
+    love()
+names = ["Mia, my daughter", "Jude, my son","Patrick, my son","Olivia, my daughter","Hugo, my dog","John, my partner"]
+for name in names:
+    family(name)
+
+
+
+
+
+##  METHODS exercises
+#def add(x,y):   #def creates the method; add is the name of the method
+#    print(x+y)
+#number = 4      #set variable
+#add(500, number)
+
+#def hello(name):
+#    print("Hi my name is " + name)
+#hello("Michelle")
+#names = ["Mia", "Jude","Patrick","Olivia","Hugo","Sexy Daddy"]
+#for name in names:
+#    hello(name)
+
+##  Dictionary exercises
+
+#my_address = {
+#"first_name": "Michelle",
+#"last_name": "Dalmau",
+#"street": "3006 S. Olcott Blvd",
+#"city": "Bloomington",
+#"state": "Indiana",
+#"zip": "47401"
+#}
 
 #print(my_address['first_name'] + " " + my_address['last_name'])
 #print(my_address['street'])
 #print(my_address['city'] + ", " + my_address['state'] + "  " + my_address['zip'])
+
+##    LOOPS exercises
 
 #fruits = ['apple', 'kiwi','orange']
 #for fruit in fruits:
@@ -21,15 +74,28 @@ my_address = {
 #else:
 #    print("Not bad.")
 
-age = 1
-if age > 0 and age <=  2:
-    print("baby")
-elif age > 2 and age < 18:
-    print("child")
-else:
-    print("adult")
+#age = 1
+#if age > 0 and age <=  2:
+#    print("baby")
+#elif age > 2 and age < 18:
+#    print("child")
+#else:
+#    print("adult")
+
+#counter = 0
+#while counter < 5:
+#    print(counter)
+#    counter += 1
+#ctrl + c ends a loop
+
+##           SLICE Exercises
+#alphabet = "abcdefghijklmnopqrstuvwxyz"
+#print(alphabet[2:7]) #range of characters
+#print(alphabet[-2:]) #last two characters in the string
 
 
+
+##  COMPREHNSION exercises
 #results = []
 #for item in fruits:
 #    results.append(item)
